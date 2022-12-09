c1d47c3870bd5c0d971d38544dfb9cb6c337138b
零式的天空 <246009756@qq.com>
2022-12-09 15:23:20 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -66,7 +66,7 @@ cat <<EOF >/etc/v2ray/config.json
   "inbounds": [
     {
       "listen": "0.0.0.0",
-      "port": 80,
+      "port": 443,
       "protocol": "vmess",
       "settings": {
         "clients": [
